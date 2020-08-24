d25fd41b8e1cc0e85b28d8dcca5406b8bfe77d15
James Scheffler <jimmischeff@gmail.com>
2020-08-24 18:10:29 -0500

    added carousel to view images

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log('linked')
 $('form').on('submit',(event) => {
     event.preventDefault();
-    $('#container').empty()
+    $('#imgContainer').empty()
     const userInput = $('input').val()
     // console.log(userInput)
 
@@ -10,13 +10,37 @@ $('form').on('submit',(event) => {
     }).then(
         (data) => {
             for (let i = 0; i < data.results.length; i++) {
-            const $charBio = $('<div id=charBio>')
+            const $charBio = $('<div class=charBio>')
             $charBio.html(data.results[i].name)
-            $charBio.html(`<img src="${data.results[i].image}" width="100px">`)
-            $('#container').append($charBio)
+            $charBio.html(`<img src="${data.results[i].image}">`)
+            $('#imgContainer').append($charBio)
             console.log(data.results[i].name)
             }
             console.log(data.results[0].image)
+
+            let currentImgIndex=0;
+            let highestIndex=$('.charBio').length-1;
+            
+            $('#nextBtn').on('click',() => {
+                console.log('next button')
+                $('#imgContainer').children().eq(currentImgIndex).css('display','none')
+                if(currentImgIndex < highestIndex) {
+                    currentImgIndex++
+                } else {
+                    currentImgIndex = 0;
+                }
+                $('#imgContainer').children().eq(currentImgIndex).css('display','block')
+            });
+            $('#prevBtn').on('click',() => {
+                console.log('next button')
+                $('#imgContainer').children().eq(currentImgIndex).css('display','none')
+                if(currentImgIndex < highestIndex) {
+                    currentImgIndex--
+                } else {
+                    currentImgIndex = 0;
+                }
+                $('#imgContainer').children().eq(currentImgIndex).css('display','block')
+            });
         }
     )
 })
